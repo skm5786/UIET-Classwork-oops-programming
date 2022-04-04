7abc564d5043fab34bad38d871b970a681ea3042
skm5786 <sumitthakral3@gmail.com>
2022-04-04 09:26:20 +0530

    Initial Commit

---
+++ UIET Classwork oops programming/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  UIET Classwork oops programming
+//
+//  Created by sumit thakral on 04/04/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
