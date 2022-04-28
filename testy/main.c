61b14aac77a5c091cb91cc45d755015e9e47b346
skm5786 <sumitthakral3@gmail.com>
2022-04-28 15:55:20 +0530

    commit

---
+++ testy/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  testy
+//
+//  Created by sumit thakral on 28/04/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
